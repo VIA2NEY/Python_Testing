8463351c0af8bbe4d090e91e302f791daca6e63d
VIA2NEY <kouameangeV11@gmail.com>
2023-03-03 23:25:08 +0000

    Qualiter du code

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,5 @@
-from locust import HttpUser, task,between
+from locust import HttpUser, task, between
+
 
 class AppUser(HttpUser):
     wait_time = between(2, 5)
@@ -9,8 +10,8 @@ class AppUser(HttpUser):
 
     @task
     def test_showSummary(self):
-        from_data = {'email':'admin@irontemple.com'}
-        self.client.post("/showSummary", data = from_data)
+        from_data = {'email': 'admin@irontemple.com'}
+        self.client.post("/showSummary", data=from_data)
 
     @task
     def test_book(self):
@@ -18,13 +19,13 @@ class AppUser(HttpUser):
 
     @task
     def test_purchasePlaces(self):
-        from_data = {'competition': 'atam competiton',
-                'club': 'Simply Lift',
-                'date': '2023-03-30 13:30:00',
-                'places':'10'}
-        self.client.post("/purchasePlaces", data = from_data)
+        from_data = {
+            'competition': 'atam competiton',
+            'club': 'Simply Lift',
+            'date': '2023-03-30 13:30:00',
+            'places': '10'}
+        self.client.post("/purchasePlaces", data=from_data)
 
-    
     @task
     def test_logout(self):
         self.client.get("/logout")
--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 import datetime
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 
 def loadClubs():
     with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+        listOfClubs = json.load(c)['clubs']
+        return listOfClubs
 
 
 def loadCompetitions():
     with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+        listOfCompetitions = json.load(comps)['competitions']
+        return listOfCompetitions
 
 
 app = Flask(__name__)
@@ -21,43 +21,53 @@ app.secret_key = 'something_special'
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
+
+@app.route('/showSummary', methods=['POST'])
 def showSummary():
     try:
-        club = [club for club in clubs if club['email'] == request.form['email']][0]
-        return render_template('welcome.html',club=club,competitions=competitions)
+        club = [club for club in clubs
+                if club['email'] == request.form['email']][0]
+        return render_template('welcome.html',
+                               club=club,
+                               competitions=competitions)
     except IndexError:
         erreur = 1
-        return render_template('index.html',erreur=erreur)
-    
+        return render_template('index.html', erreur=erreur)
 
 
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
+def book(competition, club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        return render_template('booking.html', club=foundClub,
+                               competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club,
+                               competitions=competitions)
 
 
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
     try:
-        competition = [c for c in competitions if c['name'] == request.form['competition']][0]
+        competition = [c for c in competitions
+                       if c['name'] == request.form['competition']][0]
         club = [c for c in clubs if c['name'] == request.form['club']][0]
         competitions_with_valid_date = []
         competitions_done = []
 
         for comp in competitions:
             date_str = str(comp['date'])
-            date_object = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+            date_object = datetime.datetime.strptime(
+                date_str, '%Y-%m-%d %H:%M:%S')
+
             if date_object > datetime.datetime.now():
                 comp['date'] = date_object
                 competitions_with_valid_date.append(comp)
@@ -68,17 +78,26 @@ def purchasePlaces():
 
         if places:
             placesRequired = int(places)
-            if (placesRequired <= int(competition['numberOfPlaces']) and placesRequired > 0) and (int(club['points'])>0 and (int(club['points']) >= placesRequired and placesRequired < 13)):
-                competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+            if (placesRequired <= int(competition['numberOfPlaces'])
+                and placesRequired > 0) and (int(club['points']) > 0
+                                             and (int(club['points'])
+                                                  >= placesRequired and
+                                                  placesRequired < 13)):
+                competition['numberOfPlaces'] = int(
+                    competition['numberOfPlaces']) - placesRequired
                 club['points'] = int(club['points']) - placesRequired
-                flash('Felicitation! vous venez de reserver {} places'.format(placesRequired))
+                flash('Felicitation! vous venez de reserver {} places'
+                      .format(placesRequired))
             else:
-                flash('echec lors de la reservation de {} places !'.format(placesRequired))
+                flash('echec lors de la reservation de {} places !'
+                      .format(placesRequired))
         else:
             flash('Aucune place n\'a été sélectionnée!')
-        return render_template('welcome.html', club=club, competitions=competitions_with_valid_date, competition_done=competitions_done)
+        return render_template('welcome.html', club=club,
+                               competitions=competitions_with_valid_date,
+                               competition_done=competitions_done)
     except Exception as e:
-         print(e)
+        print(e)
 
 
 # TODO: Add route for points display
